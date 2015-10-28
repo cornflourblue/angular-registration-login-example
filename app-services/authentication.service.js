712c2303d6394b078a98b9dcebc55fe0c183e25c
gp4pgit <gianluigi.perrone@pgit.de>
2015-10-28 01:15:22 +0100

    removed blank from 'Basic' authorisation headers …
    This fixes the following error:
    [Error] Error: SyntaxError: DOM Exception 12 setRequestHeader@[native code]...

--- app-services/authentication.service.js
+++ app-services/authentication.service.js
@@ -58,7 +58,7 @@
         function ClearCredentials() {
             $rootScope.globals = {};
             $cookieStore.remove('globals');
-            $http.defaults.headers.common.Authorization = 'Basic ';
+            $http.defaults.headers.common.Authorization = 'Basic';
         }
     }
 
